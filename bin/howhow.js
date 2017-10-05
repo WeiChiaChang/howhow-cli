b98d0d79649dae7f85a0118c7b0d9ae635e6473a
WeiChiaChang <ioh04019@gmail.com>
2017-10-05 11:28:24 +0800

    update

--- bin/howhow.js
+++ bin/howhow.js
@@ -1,24 +1,23 @@
 #!/usr/bin/env node  
-var fs = require("fs"),
+let fs = require("fs"),
     path = process.cwd();
 
-var howhow_quotes = [
-    "J 個是～", "陳孜昊你北七喔！"
-]
+let quotes = JSON.parse(fs.readFileSync('../src/quotes.json', 'utf8'));
+// console.log(quotes);
 
-var getRandomInt = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1) + min)
+let getRandomInt = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-var run = function (obj) {
+let run = function (obj) {
     if (obj[0] === '-v') {
         console.log('version is 1.0.0');
     } else if (obj[0] === '-h') {
         console.log('Usage:');
         console.log('  -v --version [show version]');
     } else {
-        console.log(howhow_quotes[getRandomInt(0, howhow_quotes.length - 1)]);
-      };
+        console.log(quotes.howhow[getRandomInt(0, quotes.howhow.length - 1)]);
+    };
 };
 
 run(process.argv.slice(2)); 
