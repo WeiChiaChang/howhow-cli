afad34f50da39914a2179946fe11bfe6299e7784
WeiChiaChang <ioh04019@gmail.com>
2017-10-05 12:27:57 +0800

    🐛 Fix: pdate example version

--- bin/howhow.js
+++ bin/howhow.js
@@ -4,9 +4,13 @@ const meow = require('meow');
 
 let quotes = {
     "howhow": [
-      "J 個是～", "更！陳孜昊你北七喔！", "你還是進監獄裡好好反省吧！",
-      "喂？小明啊 回家粗喚啦", "昊昊啊 緊來呷水果～", "喔喔喔喔喔喔喔原來是旺梨小鎮的土鳳梨酥啊！",
-      "要到了學妹的臉書，可是為什麼我要上線她就下線了 可...可惡！"
+      "J 個是～", 
+      "更！陳孜昊你北七喔！", 
+      "你還是進監獄裡好好反省吧！",
+      "喂？小明啊～回家粗喚啦～", 
+      "昊昊啊 緊來呷水果～", 
+      "喔喔喔喔喔喔喔原來是旺梨小鎮的土鳳梨酥啊！",
+      "要到了學妹的臉書，可是為什麼我要上線她就下線了，可...可惡！"
     ]
 };
 // console.log(quotes);
@@ -22,13 +26,14 @@ const cli = meow(`
     $ howhow
     喔喔喔喔喔喔喔原來是旺梨小鎮的土鳳梨酥啊！
 
+    Show version
     $ howhow -v
-    version is 1.0.30
+    version is 1.0.34
 `);
 
 let run = function (obj) {
     if (obj[0] === '-v') {
-        console.log('version is 1.0.26');
+        console.log('version is 1.0.34');
     } else if (obj[0] === '--help') {
         console.log(cli);
     } else {
