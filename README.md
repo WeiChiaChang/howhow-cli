689a78b0b7190e8a2107ce813f85669769b97190
WeiChiaChang <ioh04019@gmail.com>
2017-10-05 15:08:02 +0800

    🎨 Style: add chalk

--- README.md
+++ README.md
@@ -23,7 +23,7 @@ howhow -h
 
   Show version
   $ howhow -v
-  version is 1.0.52
+  version is 1.0.54
 
   Open howhow youtube channel with Google Chrome
   $ howhow open
--- bin/howhow.js
+++ bin/howhow.js
@@ -2,6 +2,7 @@
 const fs = require("fs");
 const meow = require('meow');
 const opn = require('opn');
+const chalk = require('chalk');
 const VERSION = meow().pkg.version
 
 let quotes = {
@@ -17,7 +18,11 @@ let quotes = {
       "喂？小明啊～回家粗喚啦～", 
       "昊昊啊 緊來呷水果～", 
       "喔喔喔喔喔喔喔原來是旺梨小鎮的土鳳梨酥啊！",
-      "要到了學妹的臉書，可是為什麼我要上線她就下線了，可...可惡！"
+      "要到了學妹的臉書，可是為什麼我要上線她就下線了，可...可惡！",
+      "等等，我只是沒有穿褲子啊，為什麼要逮捕我？",
+      "嚕拉拉嚕拉拉～嚕拉嚕拉勒～嚕拉～嚕拉～嚕拉～嚕拉嚕拉勒～",
+      "騙你的啦，我根本沒穿內褲喔！喔！差點就被你騙了呢！",
+      "孔雀餅乾，掉在地上七秒之內撿起來，更好吃"
     ]
 };
 // console.log(quotes);
@@ -34,7 +39,7 @@ const cli = meow(`
 
     Show version
     $ howhow -v
-    version is 1.0.52
+    version is 1.0.54
 
 
     Open howhow youtube channel with Google Chrome
@@ -49,7 +54,7 @@ let run = function (obj) {
     } else if (obj[0] === 'open') {
         opn('https://www.youtube.com/user/jasonjason1124', {app: 'google chrome'});
     } else {
-        console.log(quotes.howhow[getRandomInt(0, quotes.howhow.length - 1)]);
+        console.log(chalk.cyan(quotes.howhow[getRandomInt(0, quotes.howhow.length - 1)]));
     };
 };
 
